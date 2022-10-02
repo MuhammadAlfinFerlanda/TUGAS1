fd9e22b1d32265e4c1a7b320e8254bc7c6f821ac
MuhammadAlfinFerlanda <114856177+MuhammadAlfinFerlanda@users.noreply.github.com>
2022-10-02 17:09:12 +0700

    Create hewan.java

---
+++ hewan.java
@@ -0,0 +1,53 @@
+public class Hewan {
+    String mata, mulut, hidung, bulu, kaki, ekor;
+
+    public void setMata(String mata) {
+        this.mata = mata;
+}
+
+    public void setMulut(String mulut) {
+        this.mulut = mulut;
+}
+
+    public void setHidung(String hidung) {
+        this.hidung = hidung;
+}
+
+    public void setKaki(String kaki) {
+        this.kaki = kaki;
+}
+
+    public void setEkor(String ekor) {
+        this.ekor = ekor;
+}
+
+
+    public String getMata() {
+        return mata;
+}
+
+    public String getMulut() {
+        return mulut;
+}
+
+    public String getHidung() {
+        return hidung;
+}
+
+    public String getKaki() {
+        return kaki;
+}
+
+    public String getEkor() {
+        return ekor;
+}
+
+    void bermain(){
+        System.out.println("Meong");
+}
+
+    void makan(){
+        System.out.println("Meong meong meong");
+}
+
+}
